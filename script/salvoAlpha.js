522959680d13f0b8bc3ff11dfe4f30f1666a6c95
Salvatore Laisa <moebiusmania@gmail.com>
2021-07-30 11:30:10 +0200

    Updates

---
+++ script/salvoAlpha.js
@@ -0,0 +1,11 @@
+// Salvo Alpha - Plugin per jQuery
+jQuery.fn.salvoAlpha = function(quanto,durata){
+	jQuery(this).css("opacity",quanto);
+	jQuery(this).hover(passa,allontana);
+	function passa(){
+		jQuery(this).stop().animate({"opacity":1},durata);
+	}
+	function allontana(){
+		jQuery(this).stop().animate({"opacity":quanto},durata);
+	}
+}
